ca4471d1f989b6105c10c30fe980445fa8fd3ad4
ravi-tesh <raviteshchaudhary551@gmail.com>
2021-04-08 11:26:53 +0530

    comit

---
+++ Stl1.cpp
@@ -0,0 +1,21 @@
+# include<bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+T Myfunc(T x, T y){
+    
+    return (x>y)?x:y;
+    
+}
+
+
+
+int main(){
+
+cout<<"hello world\n";
+cout<< Myfunc<int>(78,34)<<endl;
+cout<<Myfunc<int>(45.89,2.34);
+
+
+}
